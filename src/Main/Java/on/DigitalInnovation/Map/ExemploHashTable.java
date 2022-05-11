b1dbb79bcbfe6b863f3ced9a76c362a179ea62c4
RAFAEL BONDIOLI DE PAULA <rafaeltecsuporte@gmail.com>
2022-05-11 17:30:24 -0300

    Incremento . termino

--- src/Main/Java/on/DigitalInnovation/Map/ExemploHashTable.java
+++ src/Main/Java/on/DigitalInnovation/Map/ExemploHashTable.java
@@ -1,6 +1,43 @@
 package Main.Java.on.DigitalInnovation.Map;
 
+import java.util.Hashtable;
+import java.util.Map;
+
 public class ExemploHashTable {
-    static void main(String[]arg
-    )
+    public static void main(String[]arg){
+        Hashtable<String, Integer> estudantes = new Hashtable<>();
+        estudantes.put("Carlos", 21);
+        estudantes.put("Mariana", 33);
+        estudantes.put("Rafaela", 18);
+        estudantes.put("Pedro", 44);
+
+        System.out.println(estudantes);
+
+        estudantes.put("Pedro", 55);
+        System.out.println(estudantes);
+
+        //remove um estudante de indice zero
+        estudantes.remove("Pedro");
+        System.out.println(estudantes);
+
+        //recupera um estudante no indice 2
+        int idadeEstudante = estudantes.get("Mariana");
+
+        System.out.println(idadeEstudante);
+        System.out.println(estudantes.size());
+
+        //navega nos registros do mapa
+
+        for (Map.Entry<String, Integer>entry: estudantes.entrySet()){
+            System.out.println(entry.getKey()+"--"+entry.getValue());
+        }
+
+        System.out.println(idadeEstudante);
+
+        for(String key : estudantes.keySet()){
+            System.out.println(key+"--"+ estudantes.get(key));
+        }
+        System.out.println(idadeEstudante);
+
+    }
 }
