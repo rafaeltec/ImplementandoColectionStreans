777d66a1055c8feb63ab802175923757426669be
RAFAEL BONDIOLI DE PAULA <rafaeltecsuporte@gmail.com>
2022-05-13 11:42:29 -0300

    Incremento

--- src/Main/Java/on/DigitalInnovation/Comparators/ComparatorsExampleList.java
+++ src/Main/Java/on/DigitalInnovation/Comparators/ComparatorsExampleList.java
@@ -2,11 +2,24 @@ package Main.Java.on.DigitalInnovation.Comparators;
 
 import com.sun.tools.javac.Main;
 
-public class ComparatorsExampleList {
-    public static void main(String[]args){
-
-        public Estudante (String nome, Integer idade){
-           
-        }
+public class Estudante implements Comparable<Estudante> {
+    private final   String nome;
+    private final  Integer idade;
+    public Estudante (String nome, Integer idade){
+        this.nome = nome;
+        this.idade = idade;
     }
+    public String getNome() {return nome; }
+    public Integer getIdade() {return idade; }
+
+    @Override
+    public String toString(){return nome+"-"+idade;}
+
+    @Override
+    public int compareTo (Estudante o){
+        return o.getIdade() - this.getIdade();
+    }
+
 }
+
+
